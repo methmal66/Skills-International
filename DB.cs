8e9e69e28dd3ef8ca9d475a40629abf12e0d89a1
Sanuja Methmal <methmal66@yahoo.com>
2022-02-22 01:40:29 +0530

    initial db connection

---
+++ DB.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Skills_International
+{
+    internal class DB
+    {
+        string connectionString;
+        SqlConnection con;
+        public DB()
+        {
+            connectionString = System.Configuration.ConfigurationManager.ConnectionStrings[1].ToString();
+            con = new SqlConnection(connectionString);
+        }
+
+        public void close()
+        {
+            con.Close();
+        }
+
+        public void execute(string sql)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.ExecuteNonQuery();
+        }
+
+        public SqlDataReader query(string sql)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand(sql, con);
+            SqlDataReader reader = cmd.ExecuteReader();
+            return reader;
+        }
+    }
+}

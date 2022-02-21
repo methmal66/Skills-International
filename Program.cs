d94141fc265ada593acdca3442fe38b803e01ce0
Sanuja Methmal <methmal66@yahoo.com>
2022-02-22 01:42:11 +0530

    launch Login page at the begining

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Skills_International
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Dashboard());
+            Application.Run(new Login());
         }
     }
 }

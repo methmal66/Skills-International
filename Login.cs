dd0be6afcf78fc57813cb4d01d5fb83604076c6e
Sanuja Methmal <methmal66@yahoo.com>
2022-02-22 08:25:33 +0530

    change variable and method names

--- Login.cs
+++ Login.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Skills_International
@@ -22,38 +15,6 @@ namespace Skills_International
             username.Clear();
             password.Clear();
         }
-
-        private void Login_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void groupBox1_Enter(object sender, EventArgs e)
-           
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Login_FormClosed(object sender, FormClosedEventArgs e)
-        {
-            
-        }
-
         private void loginBtn_Click(object sender, EventArgs e)
         {
             User user = new User();
